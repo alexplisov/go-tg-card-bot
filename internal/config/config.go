9ff8810e1ab77a5cbb89792f6e950d6e6634bde9
Alexey Plisov <alexplisov@gmail.com>
2025-08-12 19:06:14 +0500

    added validation for config field

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,19 +1,21 @@
 package config
 
 import (
+	"log/slog"
+
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	TelegramBotToken string `env:"TELEGRAM_BOT_TOKEN"`
-	OwnerChatID      int64  `env:"OWNER_CHAT_ID"`
+	TelegramBotToken string `env:"TELEGRAM_BOT_TOKEN,notEmpty,required"`
+	OwnerChatID      int64  `env:"OWNER_CHAT_ID,notEmpty,required"`
 }
 
 func LoadConfig() (*Config, error) {
 	var cfg Config
 	if err := godotenv.Load(); err != nil {
-		return nil, err
+		slog.Warn("Con't load .env file", slog.Any("warn", err))
 	}
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
